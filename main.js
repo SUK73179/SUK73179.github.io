9a36c435823e162a982ce04a5821954e4235a4b9
SUK73179 <SUK73179@github.com>
2020-03-09 19:00:37 +0700

    ire emblem 13
    eo hieu

---
+++ main.js
@@ -0,0 +1,8 @@
+$(function(){
+	$('[data-toggle="tooltip"]').tooltip();
+});
+$(function(){
+	$('#Galeforce').tooltip({title: 
+'Thêm 1 lần đánh nếu hạ gục kẻ dịch trong lượt của người chơi.' + 
+'<b> Chỉ kích hoạt 1 lần duy nhất mỗi lượt </b>', html: true, placement: "Top"}); 
+});
